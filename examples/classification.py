2d270230e9a2d4df62d839e133c8fea369715be9
tiagodsilva <tdsh97@gmail.com>
2026-02-07 10:56:40 +0400

    update

--- examples/classification.py
+++ examples/classification.py
@@ -1,11 +1,16 @@
+from functools import partial
+
 import flax.nnx as nnx
+import flax.struct as struct
 import jax
 import jax.numpy as jnp
 import optax
 import typer
+from flax.nnx.graph import GraphDef
 from sklearn.datasets import make_classification
 
-from rala.models import MLP, Linear
+from rala.laplace import laplace_approximation
+from rala.models import MLP
 from rala.train import split, train
 
 app = typer.Typer(pretty_exceptions_enable=False)
@@ -18,15 +23,74 @@ def loss_fn(y_pred: jax.Array, y_true: jax.Array):
     return loss
 
 
+def reconstruct_model(last_layer: struct.PyTreeNode, model: nnx.Module):
+    state = nnx.state(model)
+    graphdef = nnx.graphdef(model)
+    state["linear_out"] = last_layer
+    model_from_layer = nnx.merge(graphdef, state)
+    return model_from_layer
+
+
 def acc(model: MLP, X: jax.Array, y: jax.Array):
     y_pred = model(X)
     y_pred = jnp.argmax(y_pred, axis=1)
     return (y_pred == y).mean()
 
 
+@partial(jax.vmap, in_axes=(0, None, None), out_axes=0)
+def pm(samples: jax.Array, graphdef: GraphDef, X: jax.Array):
+    model = nnx.merge(graphdef, samples)
+    logits = model(X)
+    logits = nnx.log_softmax(logits, axis=1)
+    return logits
+
+
+@partial(jax.vmap, in_axes=(0, None, None), out_axes=0)
+def pm_from_last_layer(samples_layer: jax.Array, model: jax.Array, X: jax.Array):
+    model = reconstruct_model(samples_layer, model)
+    logits = model(X)
+    logits = nnx.log_softmax(logits, axis=1)
+    return logits
+
+
+def acc_from_pm(pred_m: jax.Array, y: jax.Array):
+    pred_m = jax.nn.logsumexp(pred_m, axis=0) - jnp.log(pred_m.shape[0])
+    pred = pred_m.argmax(axis=1)
+    return (pred == y).mean()
+
+
+@jax.jit
+def log_p(model: nnx.Module, X: jax.Array, y: jax.Array, sigma_p: float = 1):
+    # We compute the likelihood for data point
+    logits = model(X)
+    logits = nnx.log_softmax(logits, axis=1)
+    rows = jnp.arange(len(logits))
+    log_likelihood = logits[rows, y]
+
+    # We only want to regularize 'Param' types (weights/biases), not 'BatchStat'
+    params = nnx.state(model, nnx.Param)
+
+    l2_norm = jax.tree_util.tree_reduce(
+        lambda acc, p: acc + jnp.sum(p**2),
+        params,
+        initializer=0.0,
+    )
+
+    log_prior = -0.5 * (1 / sigma_p**2) * l2_norm
+
+    return (log_likelihood + log_prior).sum()
+
+
+@jax.jit
+def log_p_from_last_layer(last_layer: nnx.Module, model: nnx.Module, X: jax.Array, y: jax.Array):
+    last_layer_state = nnx.state(last_layer)
+    model_from_layer = reconstruct_model(last_layer_state, model)
+    return log_p(model_from_layer, X, y)
+
+
 @app.command()
 def main(
-    size: int = 1000,
+    size: int = 500,
     feat: int = 20,
     classes: int = 2,
     dmid: int = 64,
@@ -42,8 +106,31 @@ def main(
     model = MLP(feat, dmid, classes, rngs=rngs)
     model, _, _ = train(model, epochs, X_train, y_train, loss_fn, batch_size, rngs())
 
-    # Compute accuracy on the training set
+    # Compute accuracy on the test set
     jax.debug.print("{}", acc(model, X_test, y_test))
+
+    key1, key2 = jax.random.split(rngs(), 2)
+
+    # Compute Laplace approximation for the last layer
+    samples_layer, _, _ = laplace_approximation(
+        partial(log_p_from_last_layer, model=model, X=X_train, y=y_train),
+        model.linear_out,
+        key1,
+        num_samples=512,
+    )
+
+    # Compute the accuracy based on the Laplace approximation for the last layer
+    jax.debug.print("{}", acc_from_pm(pm_from_last_layer(samples_layer, model, X_test), y_test))
+
+    samples, graphdef, _ = laplace_approximation(
+        partial(log_p, X=X_train, y=y_train),
+        model,
+        key2,
+        num_samples=512,
+    )
+
+    # Compute accuracy from the predictive marginal
+    jax.debug.print("{}", acc_from_pm(pm(samples, graphdef, X_test), y_test))
 
     return model
 
---
+++ rala/laplace.py
@@ -0,0 +1,63 @@
+from functools import partial
+from typing import Callable
+
+import flax.nnx as nnx
+import flax.struct as struct
+import jax
+import jax.numpy as jnp
+import jax.scipy as jsp
+import jax.tree_util as tree_util
+from jax.flatten_util import ravel_pytree
+
+
+@partial(jax.jit, static_argnames=("unravel",))
+@partial(jax.vmap, in_axes=(0, None, None, None), out_axes=0)
+def tree_multivariate_normal(
+    key: jax.Array,
+    mean_tree: struct.PyTreeNode,
+    cov_L_tree: struct.PyTreeNode,
+    unravel: callable,
+):
+    def sample_leaf(key: jax.Array, mean: jax.Array, cov_L: jax.Array):
+        z = jax.random.normal(key, mean.shape)
+        flat_mean = mean.ravel()
+        delta = jsp.linalg.solve_triangular(cov_L, z, lower=True, trans=1)
+        return (flat_mean + delta).reshape(mean.shape)
+
+    # Convert keys into the structure of `mean_tree`
+    treedef = tree_util.tree_structure(mean_tree)
+    keys = jax.random.split(key, treedef.num_leaves)
+    key_tree = tree_util.tree_unflatten(treedef, keys)
+
+    theta = tree_util.tree_map(
+        sample_leaf,
+        key_tree,
+        mean_tree,
+        cov_L_tree,
+    )
+    return unravel(theta)
+
+
+def laplace_approximation(
+    log_p: Callable[[nnx.Module], float],
+    model: nnx.Module,
+    key: jax.Array,
+    num_samples: int,
+):
+    state = nnx.state(model)
+    graphdef = nnx.graphdef(model)
+    theta, unravel = ravel_pytree(state)
+
+    @jax.jit
+    def log_p_flat(theta):
+        state_unflat = unravel(theta)
+        model_reconstructed = nnx.merge(graphdef, state_unflat)
+        return log_p(model_reconstructed)
+
+    hessian = -jax.hessian(log_p_flat)(theta)
+    hessian_L = jnp.linalg.cholesky(hessian)
+
+    key, *keys = jax.random.split(key, num_samples + 1)
+    keys = jnp.array(keys)
+    # We assume independence between layers for multi-layered models
+    return tree_multivariate_normal(keys, theta, hessian_L, unravel), graphdef, key
